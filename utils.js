ae151388b18cf92505697ca5cbea9148c0f496d6
superbuggy <jpqreichard@gmail.com>
2017-01-15 15:35:21 -0500

    improve formatting

--- utils.js
+++ utils.js
@@ -6,37 +6,6 @@ function getClassroomSchedule (n){
   return axios.get(`https://raw.githubusercontent.com/ga-dc/${n}/master/schedule.yml`)
 }
 
-function parseYamlSchedule(response, classRoomNum) {
-
-  if (response.status >= 400) {
-    throw new Error("Bad response from server");
-  }
-
-  let parsedResponse = parsey.load(response.data);
-  let daysJson = parsedResponse[1].days;
-  let parsedDate = parsedResponse[0]["start-date"].replace(/-/g,"");
-
-  daysJson = daysJson.map((dayWrapper, dayIndex)=>{
-    let weekIndex = Math.floor(dayIndex / 5);
-    let dayNum = weekIndex * 7 + dayIndex % 5;
-    let date = moment(parsedDate, "YYYYMMDD").add(dayNum, "days");
-    let wordedDate = date.format("llll");
-    let sortDate = date.format("L");
-    dayWrapper.day = dayWrapper.day.map((timeSlot, timeIndex)=>{
-      for (var time in timeSlot) {
-        timeSlot[time].classroom = `Classroom ${classRoomNum}`;
-        timeSlot[time].date = wordedDate;
-        timeSlot[time].sortDate = sortDate;
-        timeSlot[time].time = time;
-      }
-      return timeSlot[time];
-    })
-    return dayWrapper.day;
-  })
-  .reduce(  (a,c) => a.concat(c) );
-  return daysJson;
-}
-
 function buildSchedule (){
   let axiosCalls = [ getClassroomSchedule('cr5'), getClassroomSchedule('cr6') ]
 
@@ -62,6 +31,38 @@ function sortByDate(data){
     return 0;
   })
   return data;
+}
+
+function parseYamlSchedule(response, classRoomNum) {
+
+  if (response.status >= 400) {
+    throw new Error("Bad response from server");
+  }
+
+  let parsedResponse = parsey.load(response.data);
+  let daysJson = parsedResponse[1].days;
+  let parsedDate = parsedResponse[0]["start-date"].replace(/-/g,"");
+
+  daysJson = daysJson.map((dayWrapper, dayIndex)=>{
+    let weekIndex = Math.floor(dayIndex / 5);
+    let dayNum = weekIndex * 7 + dayIndex % 5;
+    let date = moment(parsedDate, "YYYYMMDD").add(dayNum, "days");
+    let wordedDate =
+      date.format("llll").split(" ").splice(0,4).join(" ").replace(/,/g, "");
+    let sortDate = date.format("L");
+    dayWrapper.day = dayWrapper.day.map((timeSlot, timeIndex)=>{
+      for (var time in timeSlot) {
+        timeSlot[time].classroom = `Classroom ${classRoomNum}`;
+        timeSlot[time].date = wordedDate;
+        timeSlot[time].sortDate = sortDate;
+        timeSlot[time].time = time;
+      }
+      return timeSlot[time];
+    })
+    return dayWrapper.day;
+  })
+  .reduce(  (a,c) => a.concat(c) );
+  return daysJson;
 }
 
 module.exports = {
