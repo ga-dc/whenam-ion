6b308159e0102be01ebc7b1a9575349c7c322ea0
superbuggy <jpqreichard@gmail.com>
2017-01-15 14:32:55 -0500

    add sorting by date to back-end

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const cors = require('cors');
 app.use(cors());
 
 app.get('/schedule', (req, res)=> {
-  utils.buildSchedule().then( data => res.send(data) );
+  utils.buildSchedule().then( data => {
+    data = data.sort((a,b)=>{
+      if (a.sortDate < b.sortDate) {
+        return -1;
+      }
+      if (a.sortDate > b.sortDate) {
+        return 1;
+      }
+      return 0;
+    })
+    res.send(data)
+  });
 });
 
 app.listen(3007, ()=>{
